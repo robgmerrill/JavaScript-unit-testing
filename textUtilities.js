2ecf85ff5926cb70180c882cac4d87f726188656
Rob Merrill <robgmerrill@gmail.com>
2016-04-26 14:53:22 -0700

    testing title case

--- textUtilities.js
+++ textUtilities.js
@@ -5,7 +5,16 @@ var expect = require('chai').expect;
 expect(true).to.be.true;
 
 function titleCase(title) {
-  return title;
-};
+  var words = title.split(' ');
+  var titleCasedWords = words.map(function(word) {
+    return word[0].toUpperCase() + title.substring(1);
+  });
+
+
+  return titleCasedWords.join(' ');
 
 expect(titleCase('the great mouse detective')).to.be.a('string');
+expect(titleCase('a')).to.equal('A');
+expect(titleCase('vertigo')).to.equal('Vertigo');
+
+expect(titleCase('the great mouse detective')).to.equal('The Great Mouse Detective');
